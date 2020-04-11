cb63f06970493227c2841751fe86e0ccd6ab83e8
tanaton <heiwaboke@gmail.com>
2020-04-11 14:06:22 +0900

    ファイル追加

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+	//mode: 'development', 
+	mode: 'production',
+	entry: {
+		treemap: './ts/treemap.ts',
+		line: './ts/line.ts'
+	},
+	output: {
+		path: path.join(__dirname,'www/js'),
+		filename: '[name].js'
+	},
+	resolve: {
+		extensions:['.ts','.js'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	module: {
+		rules: [
+			{
+				// 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
+				test:/\.ts$/,loader:'ts-loader'
+			}
+		]
+	}
+}
